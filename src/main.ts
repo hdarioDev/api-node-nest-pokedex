71352cb40729d16f7cae4f207b5e75c6451d92ec
hdariodev <hdariodev@gmail.com>
2024-02-10 07:29:16 -0400

    feat: image mongo created

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('ap/v1');
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 main();
