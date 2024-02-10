61a6889ce105f2f80e7d69cbf6c78007fc7d6693
hdariodev <hdariodev@gmail.com>
2024-02-10 08:13:29 -0400

    feat: pokemon model, schema created

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
+  imports: [
+    // 👇 import the MongooseModule
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
 })
 export class PokemonModule {}
